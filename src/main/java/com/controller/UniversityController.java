a6d79e75ba5526c7672ab45b638ef8245b07b0d3
Roberto Gomez <roberto.gomez02@globant.com>
2025-02-20 18:46:05 -0500

    fix: Refactored UniversityController

--- src/main/java/com/controller/UniversityController.java
+++ src/main/java/com/controller/UniversityController.java
@@ -15,31 +15,8 @@ public class UniversityController {
         this.university = university;
     }
 
-    public void addStudentAndAddToClass(Student student, Course course) {
-        university.addStudentToUniversity(student);
-        course.addStudent(student);
+    public University getUniversity() {
+        return university;
     }
 
-    public void addTeacherToUniversity(Teacher teacher) {
-        university.addTeacherToUniversity(teacher);
-    }
-
-    public void addNewCourseToUniversity(Course course, List<Student> studentList) {
-        university.addCourseToUniversity(course);
-        for (Student student: studentList) {
-            course.addStudent(student);
-        }
-    }
-    public List<Course> getStudentClasses(Student student) {
-        List<Course> studentCourses = new ArrayList<>();
-        outer: for (Course course: university.getCourseList()) { // Linear search
-            for (Student temporalStudent: course.getStudentList()) {
-                if (temporalStudent == student) {
-                    studentCourses.add(course);
-                    continue outer;
-                }
-            }
-        }
-        return studentCourses;
-    }
 }
