921dc4307c09d02630a209b76348154dc712254f
Roberto Gomez <roberto.gomez02@globant.com>
2025-02-20 14:55:25 -0500

    feat: Added Person and Student classes

---
+++ src/main/java/com/base/Student.java
@@ -0,0 +1,13 @@
+package com.base;
+
+public class Student {
+    private final int studentId;
+
+    public Student(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+}
