4190ffc72e7d6cfbcac86d53138616d507ee5f74
Roberto Gomez <roberto.gomez02@globant.com>
2025-02-21 14:50:42 -0500

    fix: Teacher title when printing teachers

--- src/main/java/com/utils/MenuViewUtils.java
+++ src/main/java/com/utils/MenuViewUtils.java
@@ -1,7 +1,12 @@
 package com.utils;
 
+import com.model.Teacher;
+
 public class MenuViewUtils {
     public static <T> String getItemType(T item) {
+        if (item instanceof Teacher) {
+            return Teacher.class.getSimpleName();
+        }
         return item.getClass().getSimpleName();
     }
 }
