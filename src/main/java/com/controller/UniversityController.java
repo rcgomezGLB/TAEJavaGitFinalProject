5fc2ea8f7ebc94cbb810c68a22243f4e950b31b1
Roberto Gomez <roberto.gomez02@globant.com>
2025-02-24 08:58:42 -0500

    fix: unnecessary imports

--- src/main/java/com/controller/UniversityController.java
+++ src/main/java/com/controller/UniversityController.java
@@ -1,12 +1,6 @@
 package com.controller;
 
-import com.model.Course;
-import com.model.Student;
-import com.model.Teacher;
 import com.model.University;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class UniversityController {
     private final University university;
